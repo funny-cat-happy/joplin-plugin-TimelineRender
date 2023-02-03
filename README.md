a2549205425546df87c544a00d500eda127a3321
jucat <fanyingjiyun@foxmail.com>
2023-02-03 21:01:06 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# joplin-plugin-TimelineRender
+
+render the timeline in the markdown
+
+![](./photo/result.png)
+
+# Usage
+
+````
+```timeline
+2022-1-1
+build project
+build folder
+
+2023-2-2
+develop map generate and basic AI
+
+2023-2-3
+test map
+```
+````
+The block start with \`\`\`timeline,end with \`\`\`
+
+Every node is composed of time and content and is split by space line between different node.
+
+# Acknowledgements
+Hieu-Thi Luong's Life Calendar plugin,I refer to his code.
+
+乔珂力的css时间轴教程
+
+Joplin's offical tutorial.
+
+# License
+GPL
---
+++ src/TimelineRule.js
@@ -0,0 +1,33 @@
+module.exports = {
+  default: function(context) {
+    return {
+      plugin: function (markdownIt, _options) {
+        const defaultRender = markdownIt.renderer.rules.fence || function(tokens, idx, options, env, self) {
+          return self.renderToken(tokens, idx, options, env, self);
+        };
+        markdownIt.renderer.rules.fence = function(tokens, idx, options, env, self) {
+          const token = tokens[idx];
+          if (token.info !== 'timeline') 
+          {
+            return defaultRender(tokens, idx, options, env, self);
+          }
+          try {
+            var contentHtml = markdownIt.utils.escapeHtml(token.content);
+            console.info(contentHtml);
+          } catch (e) {
+            var contentHtml = {};
+          }
+          return `
+              <div class="timeline-container">${contentHtml}</div>
+          `;
+        };
+      },
+      assets: function () {
+        return [
+            { name: 'Timeline.css' },
+            { name: 'Timeline.js' }
+        ];
+      }
+    }
+  }
+}
